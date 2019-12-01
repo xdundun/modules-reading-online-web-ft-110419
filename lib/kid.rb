b06a51ca3804d7ac027828690fdb632d974ba215
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-01 21:46:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
 
   attr_accessor :name
 
